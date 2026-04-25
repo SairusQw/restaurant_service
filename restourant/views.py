cc3b02ac2fd54e61bd3351a1d7e873fde89322ce
Denis <sairustv228@gmail.com>
2026-04-26 00:49:17 +0300

    Create base templates, urls and views

--- restourant/views.py
+++ restourant/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-from restourant.models import Cook, Dish
+from restourant.models import (Cook,
+                               Dish,
+                               DishType,
+                               Ingredient)
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -15,4 +19,30 @@ def index(request: HttpRequest) -> HttpResponse:
     }
     return render(request, "restourant/index.html", context)
 
+
+class DishTypeListView(generic.ListView):
+    model = DishType
+    template_name = "restourant/dish_type_list.html"
+    context_object_name = "dish_type_list"
+
+class DishListView(generic.ListView):
+    model = Dish
+    template_name = "restourant/dish_list.html"
+    queryset = (Dish.objects.select_related("dish_type")
+                .prefetch_related("cooks"))
+    context_object_name = "dish_list"
+
+class DishDetailView(generic.DetailView):
+    model = Dish
+
+class CookListView(generic.ListView):
+    model = Cook
+    template_name = "restourant/cook_list.html"
+
+class CookDetailView(generic.DetailView):
+    model = Cook
+
+class IngredientListView(generic.ListView):
+    model = Ingredient
+    template_name = "restourant/ingredient_list.html"
 
