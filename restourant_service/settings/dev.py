9d11cd18681443a14dcb01804a7084bb6932b7d0
Denis <sairustv228@gmail.com>
2026-04-28 11:44:35 +0300

    Solution

---
+++ restourant_service/settings/dev.py
@@ -0,0 +1,16 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
